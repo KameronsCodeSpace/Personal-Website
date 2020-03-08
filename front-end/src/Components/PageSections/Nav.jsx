f9b9ae995789646c8280241e89b1616262fe40a6
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-03-08 17:58:00 -0400

    playing with nav css

--- front-end/src/Components/PageSections/Nav.jsx
+++ front-end/src/Components/PageSections/Nav.jsx
@@ -1,84 +1,83 @@
 import React from 'react';
 import { Link } from "react-scroll";
 
-
 function Nav() {
     return (
-        <div className='Navbar'>
-            <h2>KAMERON</h2>
-            <ul>
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Profile"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Profile</Link>
-                </li>
+        <div>
+            <nav className="nav">
+                <div className='logo'>
+                    <h4>KAMERON</h4>
+                </div>
+                <div>
+                <ul>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Profile"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Profile</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Projects"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Projects</Link>
-                </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Projects"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Projects</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Skills"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Skills</Link>
-                </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Skills"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Skills</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Achievements"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Achievements</Link>
-                </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Achievements"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Achievements</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Hobbies"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Hobbies</Link>
-                </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Hobbies"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Hobbies</Link>
+                    </li>
 
-                <li>
-                    <Link
-                        // className='nav-links'
-                        activeClass="active"
-                        to="Contact"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                    >Contact</Link>
-                </li>
-            </ul>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Contact"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                        >Contact</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
         </div>
     );
 }
