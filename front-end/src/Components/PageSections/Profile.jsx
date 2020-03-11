d5daacd310d18f2ede3e868c4cfd84fe75d15d57
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-03-11 10:16:37 -0400

    created a basic profile page

--- front-end/src/Components/PageSections/Profile.jsx
+++ front-end/src/Components/PageSections/Profile.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 function Profile() {
     return (
         <div className='Profile-Section' id="Profile">
-            <h1>Profile</h1>
+            <div className='align-right'>
+                <h1>Kameron Montague</h1>
+                <p>Full Stack Web Developer</p>
+                <p>
+                    Lorem ipsum dolor sit amet, etiam
+                    atomorum corrumpit eum no, sed ex
+                    imperdiet vituperatoribus, eam errem
+                    eleifend no. Nullam dictas albucius an his,
+                    pericula evertitur sea ad. Qui ea
+                    commune omnesque mediocrem, id eos
+                    tempor iudicabit. Nec latine aliquam ei.
+                </p>
+                <a href='https://docs.google.com/document/d/1jygdfu9DGSpMzfHDbWzr40ZPji99_oK0CucsfaoCiGM/edit?usp=sharing'><button className='btn'>Download Resume</button></a>
+
+            </div>
+            <div>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrrUUCpJipBHo_h3dGxcKgVeZMIn8nHT6dSMU8H3Sb-sjB2naq' alt='' />
+            </div>
+            <div className='align-left'>
+                <p>
+                    Lorem ipsum dolor sit amet, etiam
+                    atomorum corrumpit eum no, sed ex
+                    imperdiet vituperatoribus, eam errem
+                    eleifend no. Nullam dictas albucius an his,
+                    pericula evertitur sea ad. Qui ea
+                    commune omnesque mediocrem, id eos
+                    tempor iudicabit. Nec latine aliquam ei.
+                </p>
+            </div>
         </div>
     );
 }
